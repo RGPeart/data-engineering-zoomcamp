b059842dee86919d42d50d26e3d0c1910e549e8c
Ryan Peart <rgpeart@hotmail.com>
2026-01-26 03:48:29 +0000

    Organized files into modules

---
+++ 1-docker-terraform/terraform-gcp/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = "rgpeart-datatalks-de-course"
+  region      = "us-east4"
+}
