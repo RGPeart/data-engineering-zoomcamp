b8a174379a3d1cb9f0a5da658d1837105a2fe7ab
Ryan Peart <rgpeart@hotmail.com>
2026-02-15 20:44:07 +0000

    Updates to main.tf as per DE Talks course

--- 1-docker-terraform/terraform-gcp/main.tf
+++ 1-docker-terraform/terraform-gcp/main.tf
@@ -12,3 +12,20 @@ provider "google" {
   project     = "rgpeart-datatalks-de-course"
   region      = "us-east4"
 }
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "rgpeart-datatalks-de-course-terra-bucket"
+  location      = "US"
+  force_destroy = true
+
+  # Lifecycle rule to automatically delete objects after 1 day if they are not fully uploaded (incomplete multipart uploads)
+  lifecycle_rule {
+    condition {
+      # Days since the object was created
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
